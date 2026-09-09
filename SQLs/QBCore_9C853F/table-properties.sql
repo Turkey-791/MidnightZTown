39a43b33eae2914e855089e1f96f0fe84875c57d
potato <dummy>
2026-09-09 22:42:38 +0900

    [データベース]propertiesテーブル移植

---
+++ SQLs/QBCore_9C853F/table-properties.sql
@@ -0,0 +1,45 @@
+-- --------------------------------------------------------
+-- ホスト:                          127.0.0.1
+-- サーバーのバージョン:                   10.11.14-MariaDB-0ubuntu0.24.04.1 - Ubuntu 24.04
+-- サーバー OS:                      debian-linux-gnu
+-- HeidiSQL バージョン:               12.21.0.7344
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--  テーブル QBCore_7F2650.properties の構造をダンプしています
+CREATE TABLE IF NOT EXISTS `properties` (
+  `property_id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_citizenid` varchar(50) DEFAULT NULL,
+  `street` varchar(100) DEFAULT NULL,
+  `region` varchar(100) DEFAULT NULL,
+  `description` longtext DEFAULT NULL,
+  `has_access` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT json_array() CHECK (json_valid(`has_access`)),
+  `extra_imgs` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT json_array() CHECK (json_valid(`extra_imgs`)),
+  `furnitures` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT json_array() CHECK (json_valid(`furnitures`)),
+  `for_sale` tinyint(1) NOT NULL DEFAULT 1,
+  `price` int(11) NOT NULL DEFAULT 0,
+  `shell` varchar(50) NOT NULL,
+  `apartment` varchar(50) DEFAULT NULL,
+  `door_data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`door_data`)),
+  `garage_data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`garage_data`)),
+  `zone_data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`zone_data`)),
+  PRIMARY KEY (`property_id`),
+  UNIQUE KEY `UQ_owner_apartment` (`owner_citizenid`,`apartment`),
+  CONSTRAINT `FK_owner_citizenid` FOREIGN KEY (`owner_citizenid`) REFERENCES `players` (`citizenid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- エクスポートするデータが選択されていません
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
